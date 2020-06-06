8bc8c6f9143828a5e7a8b92b5475e5944a737c0c
Ayappa <ayappaganesh1995@gmail.com>
2020-06-06 15:39:30 -0400

    heroku url chnged

--- src/main/java/com/example/pollEveryWhere/pollEveryWhere/controller/UserController.java
+++ src/main/java/com/example/pollEveryWhere/pollEveryWhere/controller/UserController.java
@@ -59,7 +59,7 @@ public class UserController {
 		 String toekn=jwt.generateToken(userDetails);
 			final JwtBean jwtToken=new JwtBean(toekn);
 			//String link="http://localhost:3000/sendRegisterMail/"+toekn;
-			String link="http://pollvoicefrontend.herokuapp.com/sendRegisterMail/"+toekn;
+			String link="https://pollvoicefrontend.herokuapp.com/sendRegisterMail/"+toekn;
 			emailServiceImpl.sendSimpleMessage(user.getEmail(),"Registration","\"click on the link to login \\n "+link);
 			return jwtToken;
 	}
@@ -93,7 +93,7 @@ public class UserController {
 			}
 		String toekn=jwt.generateDummyToken(userCheck);
 		//String link="http://localhost:3000/updatePasswordMail/"+toekn;
-		String link="http://pollvoicefrontend.herokuapp.com/updatePasswordMail/"+toekn;
+		String link="https://pollvoicefrontend.herokuapp.com/updatePasswordMail/"+toekn;
 		boolean status=emailServiceImpl.sendSimpleMessage(user.getEmail(),"Update PAssword","\"click on the link to reset password : \\n "+link);
 		//emailServiceImpl.sendSimpleMessage(user.getEmail(),"Update PAssword","\"click on link to reset password : \\n Spring Boot Email\"");
 		return status?"email sent":"try again later";
